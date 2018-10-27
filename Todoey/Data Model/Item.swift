e3fbd762659f8276ed2c9eea56247f2aeed8566c
AndreasAerts <r0658500@student.thomasmore.be>
2018-10-27 14:02:38 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andreas Aerts on 27/10/2018.
+//  Copyright © 2018 Andreas Aerts. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
